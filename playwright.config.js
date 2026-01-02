0356b1bdaf727225a187fb2e78ab59917c06c384
Rahul Mahajan <rrmahajan6@gmail.com>
2026-01-02 16:18:18 +0530

    reporter changes

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html"],["line"],["allure-playwright"]],
+  //reporter: [["html"],["line"],["allure-playwright"]],
+  reporter: [["html"]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
